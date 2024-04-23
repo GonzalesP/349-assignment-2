2eff23b2f0e69df80da1de3179de43ef560355c3
P Gonz <GonzalesP@csu.fullerton.edu>
2024-04-22 22:05:34 -0700

    Finished project.

---
+++ assignment-2/src/Components/Board.jsx
@@ -0,0 +1,127 @@
+import style from '../css/game.module.css';
+
+// The Square component represents a clickable cell in the 3x3 Tic-Tac-Toe board
+// the 'value' parameter indicates what will be placed inside the square, which
+// will either be an X, O, or empty square.
+function Square({ value, onSquareClick }) {
+  return (
+    <button className={style["square"]} onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+// The Board component represents a Game components's 3x3 Tic-Tac-Toe game board
+// It is composed of 9 clickable Square components.
+// The board references 3 parameters passed down from its parent Game component.
+// xIsNext indicates whether it is player X's turn or player O's turn.
+// currentState is the current game state passed down from the Game component.
+// onPlay references a function of the Game component. this function is used to
+// record the new game state resulting from a new move made by any player.
+export default function Board({ xIsNext, currentState, onPlay }) {
+  // This function is called when a Square component is clicked (i.e. a new move
+  // is made by any player). The cellNumber parameter refers to which square was
+  // pressed (0 thru 8).
+  // Depending on whether the new move is valid or not, a new game state will
+  // be created. This new game state will have an X or O in the selected Square,
+  // and the new game state will be added to the history of game states.
+  // In order to save this new game state, the onPlay function will be used
+  // (which is passed down from the parent Game component).
+  function handleClick(cellNumber) {
+    // First, check whether the chosen move is valid or not. If the Square
+    // is already occupied, or if the game is already over, then the move
+    // is not valid.
+    if (currentState[cellNumber] || calculateWinner(currentState)) {
+      return;
+    }
+
+    // If the move is valid, create a copy of the current state. Then, update
+    // the copy to contain the new move (made by either X or O).
+    const nextState = currentState.slice();
+    if (xIsNext) {
+      nextState[cellNumber] = "X";
+    } else {
+      nextState[cellNumber] = "O"
+    }
+
+    // Lastly, save this new game state by calling the onPlay function, which
+    // will save this new state in a history variable.
+    onPlay(nextState);
+  }
+
+  // This function is used to determine whether either player has won yet. If a
+  // player has won, it will return their symbol (X or O). Otherwise, if neither
+  // player has won, the function will return null.
+  function calculateWinner(currentState) {
+    // First, create a list of the possible win conditions (3 in a row,
+    // 3 in a column, or 3 in a diagonal).
+    const winConditions = [
+      [0, 1, 2],  // top row
+      [3, 4, 5],  // middle row
+      [6, 7, 8],  // bottom row
+      [0, 3, 6],  // left column
+      [1, 4, 7],  // middle column
+      [2, 5, 8],  // right column
+      [0, 4, 8],  // top left to bottom right
+      [2, 4, 6]  // top right to bottom left
+    ];
+  
+    // Next, iterate through the list of possible win conditions. If any of them
+    // have the same symbol in each position (e.g. all X's in the top row),
+    // return the corresponding winner
+    for (const condition of winConditions) {
+      // first, get the 3 Square numbers of the win condition
+      // (e.g. 0, 1, 2 for the top row)
+      const [a, b, c] = condition
+      // if the 3 Squares of any win condition match, then that player has won
+      if (currentState[a] && currentState[a] === currentState[b] &&
+          currentState[a] === currentState[c]) {
+        
+        return currentState[a];
+      }
+    }
+
+    // Otherwise, if none of the win conditions are satisfied, neither player
+    // has won. So, return null
+    return null;
+  }
+
+  // winner indicates whether either Player has won the game or not
+  const winner = calculateWinner(currentState);
+
+  // Status is a message that indicates which player will make the next move
+  // (or alternatively, which player has won). Depending on whether winner is
+  // null or not, its text will be changed accordingly.
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  // The JSX of the Board component consists of the status message and
+  // 9 Square components. The 9 Square components are arranged in a 3x3 grid.
+  // Styles are provided by css/game.module.css
+  return (
+    <>
+      <div className={style["status-container"]}>
+        <p className={style["status-message"]}>{status}</p>
+      </div>
+      <div className={style["board-row"]}>
+        <Square value={currentState[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={currentState[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={currentState[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className={style["board-row"]}>
+        <Square value={currentState[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={currentState[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={currentState[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className={style["board-row"]}>
+        <Square value={currentState[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={currentState[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={currentState[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  )
+}
---
+++ assignment-2/src/Game.jsx
@@ -0,0 +1,117 @@
+import { useState } from "react";
+import style from './css/game.module.css';
+import Board from "./Components/Board";
+
+// The Game component represents the entire Tic-Tac-Toe game, which consists of
+// a 3x3 board and a history (list) of player moves made throughout the game.
+export default function Game() {
+  // A game state refers to the state a Tic-Tac-Toe game is in. A game state is
+  // either ongoing or finished (depending on when the game continues or not).
+  // Here is an example of a possible (ongoing) game state:
+    // X|O|X
+    // —+—+—
+    // O|X| 
+    // —+—+—
+    // O| | 
+  
+  // The 'history' state variable is a list of game states. It records all of
+  // the game states leading up to (and including) the current game state.
+  // In short, it keeps a history of player moves during each turn.
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+
+  // The 'currentMove' state variable keeps track of how many moves
+  // have been made so far in the current game state. At the beginning of the
+  // game, currentMove is 0, since 0 moves have been made.
+  const [currentMove, setCurrentMove] = useState(0);
+
+  // The 'xIsNext' state variable keeps track of whether player X gets to
+  // make the next move or not. Player X is the first to move, and
+  // then they alternate with Player O for subsequent moves (X, O, X, ...).
+  // In other words, Player X will always move when 'currentMove', aka the
+  // total number of moves made so far, is even.
+  const xIsNext = currentMove % 2 === 0;
+
+  // 'currentGameState' is a state variable that keeps track of the game's
+  // current state (i.e. its current appearance).
+  // By default, it is set to the latest game state recorded in the
+  // 'history' state. However, if a user clicks on one of the previous
+  // game states (listed as moves in the game-history <ordered list>),
+  // the currentGameState will be reverted to the selected older game state.
+  const currentGameState = history[currentMove];
+
+  // This function is called when a player makes a move (i.e. updates the
+  // current game state).
+  // The 'nextGameState' parameter represents the new game state resulting
+  // from a new move made by any player. This new game state is added to the
+  // list of game states saved in the 'history' variable.
+  // Additionally, this function is passed to the Game's Board component, which
+  // allows the Board to reference this function when needed (i.e. update the
+  // game-board when a player makes a new, valid move)
+  function handlePlay(nextGameState) {
+    // create a new history variable by adding the new (most recent) game state
+    // to the end of the current history (i.e. current list of moves/game states)
+    const nextHistory = [...history.slice(0, currentMove + 1), nextGameState];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+
+
+  // jumpTo() is called whenever a user clicks on one of the game states listed
+  // in the game-history <ordered list>. When this function executes,
+  // the 'currentMove' variable will be updated to the corresponding move that
+  // the user selected from the game-history <ol>
+  // (e.g. if they clicked on Move #3, the current game state will be reverted
+  // to match the previous game state resulting from Move #3).
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  // 'moves' is a constant that is used to render <list items> for the
+  // game-history <ordered list>. each <list item> will contain a button
+  // that can be clicked to change the currentGameState to any of the
+  // game states saved in history (i.e. go back to the game state made after
+  // a sepcific move).
+  const moves = history.map((gameState, move) => {
+    // description is the text of the <li>'s button. it describes what will
+    // happen when the button is clicked (e.g. going back to move #3)
+    let description;
+
+    // the topmost button of the game's history will revert the game to the
+    // very beginning (i.e. an empty 3x3 board).
+    if (move > 0) {
+      description = "Go to move #" + move;
+    }
+    // other buttons will revert the game to the corresponding move (#)
+    else {
+      description = "Go to game start";
+    }
+
+    // create a <li> and <button> using a JSX component and return it.
+    // this JSX will be nested in the game-history <ordered list>.
+    return (
+      <li key={move}>
+        <button className={style["history-item"]}
+          onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    )
+  });
+
+
+
+  // The following JSX represents the Game component as a whole. The Game
+  // consists of a game board and a game history. The board is created using
+  // a <Board /> component, and the history is created using an <ordered list>.
+  // The contents of the game-history <ol> is retrieved from 'moves'.
+  // Styles are provided by css/game.module.css
+  return (
+    <div className={style["game"]}>
+      <div className={style["game-board"]}>
+        <Board xIsNext={xIsNext} currentState={currentGameState} onPlay={handlePlay} />
+      </div>
+      <div className={style["game-history"]}>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  )
+}
